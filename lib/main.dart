274ec4f161df76803d76c3ba436dd7cb643b6cab
Jiinngerbread <39425899+Jiinngerbread@users.noreply.github.com>
2019-08-11 03:59:20 -0500

    testing search

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:jam_i/createalert.dart';
 import 'package:jam_i/side_drawer.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:geolocator/geolocator.dart';
 // import 'package:location/location.dart';
 
 void main() => runApp(App());
@@ -35,6 +36,8 @@ class _MyAppState extends State<MyApp> {
   LatLng _lastMapPosition = _center;
 
   MapType _currentMapType = MapType.normal;
+
+  String searchAddr;
 
   void _onMapTypeButtonPressed() {
     setState(() {
@@ -103,6 +106,14 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  searchandNavigate() {
+    Geolocator().placemarkFromAddress(searchAddr).then((result) {
+      _controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+          target:
+              LatLng(result[0].position.latitude, result[0].position.longitude),
+          zoom: 10.0)));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -234,6 +245,38 @@ class _MyAppState extends State<MyApp> {
                 myLocationEnabled: true,
               ),
             ),
+
+
+
+            Positioned(
+          top: 30.0,
+          right: 15.0,
+          left: 15.0,
+          child: Container(
+            height: 50.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0), color: Colors.white),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: 'Enter Address',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(left: 15.0, top: 15.0),
+                  suffixIcon: IconButton(
+                      icon: Icon(Icons.search),
+                      onPressed: searchandNavigate,
+                      iconSize: 30.0)),
+              onChanged: (val) {
+                setState(() {
+                  searchAddr = val;
+                });
+              },
+            ),
+          ),
+        ),
+
+
+
             Positioned(
               top: 10,
               right: 10,
